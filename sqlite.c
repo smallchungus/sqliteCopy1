6909a942cd4227224df82d3ed1ca82bcf18de100
William Chen <wchen1396@gmail.com>
2022-07-20 15:28:07 -0400

    Changing files and commmitting

--- sqlite.c
+++ sqlite.c
@@ -27,8 +27,7 @@ void print_prompt()
 
 void read_input(InputBuffer *input_buffer)
 {
-    ssize_t bytes_read =
-        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
     input_buffer->input_length = bytes_read - 1;
     input_buffer->buffer[bytes_read - 1] = 0;
